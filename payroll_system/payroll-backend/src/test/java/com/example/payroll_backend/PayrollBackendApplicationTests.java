11152f08f331e5a0ccd7c06fdf374b97bb73cbb5
Thisara Mendis <thisaramendis18@gmail.com>
2025-08-07 14:04:15 +0530

    inial setup

---
+++ payroll_system/payroll-backend/src/test/java/com/example/payroll_backend/PayrollBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payroll_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
