3d9de1b5bb9b4a20deec87436a24a7552c91b5f4
Thisara Mendis <thisaramendis18@gmail.com>
2025-08-21 21:18:37 +0530

    update

--- payroll_system/payroll-backend/src/main/java/com/example/payroll_backend/Model/EmployeeModel.java
+++ payroll_system/payroll-backend/src/main/java/com/example/payroll_backend/Model/EmployeeModel.java
@@ -1,4 +1,27 @@
 package com.example.payroll_backend.Model;
 
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employees")
 public class EmployeeModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    private LocalDate dateOfBirth;
+    private String gender;
+    private String nationalId;
+    private String email;
+    private String phoneNumber;
+
+
+
 }
