cc762888756bc6e6c90d219daa90f3ee864927c0
Thisara Mendis <thisaramendis18@gmail.com>
2025-08-22 14:41:17 +0530

    update

--- payroll_system/payroll-backend/src/main/java/com/example/payroll_backend/Model/EmployeeModel.java
+++ payroll_system/payroll-backend/src/main/java/com/example/payroll_backend/Model/EmployeeModel.java
@@ -3,6 +3,7 @@ package com.example.payroll_backend.Model;
 
 import jakarta.persistence.*;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
@@ -18,10 +19,129 @@ public class EmployeeModel {
 
     private LocalDate dateOfBirth;
     private String gender;
-    private String nationalId;
     private String email;
     private String phoneNumber;
 
+    private String address;
+    private String department;
+    private String designation;
+    private String status;   // Active, Inactive, Resigned, etc.
 
+    private LocalDate dateOfHired;
 
+    private BigDecimal basicSalary;
+
+    public EmployeeModel() {
+    }
+
+    public EmployeeModel(String lastName, String firstName, LocalDate dateOfBirth, String gender, String email, String phoneNumber, String address, String department, String designation, String status, LocalDate dateOfHired, BigDecimal basicSalary) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.department = department;
+        this.designation = designation;
+        this.status = status;
+        this.dateOfHired = dateOfHired;
+        this.basicSalary = basicSalary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getDateOfHired() {
+        return dateOfHired;
+    }
+
+    public void setDateOfHired(LocalDate dateOfHired) {
+        this.dateOfHired = dateOfHired;
+    }
+
+    public BigDecimal getBasicSalary() {
+        return basicSalary;
+    }
+
+    public void setBasicSalary(BigDecimal basicSalary) {
+        this.basicSalary = basicSalary;
+    }
 }
--- payroll_system/payroll-backend/src/main/java/com/example/payroll_backend/Service/EmployeeService.java
+++ payroll_system/payroll-backend/src/main/java/com/example/payroll_backend/Service/EmployeeService.java
@@ -1,4 +1,8 @@
 package com.example.payroll_backend.Service;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeService {
 }
